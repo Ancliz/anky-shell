b597f3876e40780a46a94a7eec184dd552a3f8d0
Ancliz <ancliz@ancliz.me>
2026-08-28 21:05:30 +1000

    fix: add missed consts for revealer flattening

--- util/gtkutil.ts
+++ util/gtkutil.ts
@@ -2,3 +2,4 @@ import { Gtk } from "ags/gtk4";
 
 export const { VERTICAL, HORIZONTAL } = Gtk.Orientation;
 export const { START, CENTER, END, FILL } = Gtk.Align;
+export const { SLIDE_RIGHT, SLIDE_DOWN } = Gtk.RevealerTransitionType;
